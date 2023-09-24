aedc58d2a4773b8fb5e7e75916622171ff1fa58b
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-09-24 17:57:38 +0530

    .

--- Advance Operating system/lab/sjf.c
+++ Advance Operating system/lab/sjf.c
@@ -1,47 +1,32 @@
-#include <stdio.h>
-struct process
-{
- int at;
- int bt;
- int ct;
- int tat;
- int wt;
-};
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+    int n;
 
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
 
-void find(struct process p[], int n)
-{
- p[0].ct = p[0].at + p[0].bt;
- p[0].tat = p[0].ct - p[0].at;
- p[0].wt = p[0].tat - p[0].bt;
+    int processes[n];
+    int arrival_time[n];
+    int burst_time[n];
+    int ct[n];
 
- 
- for (int i = 1; i < n; i++)
- {
-    int sum= p[i-1];
-    for (int j =0; j <= sum;j++)
-    {
-        
+    printf("Enter the arrival time and burst time for each process:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process %d:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &arrival_time[i]);
+        printf("Burst Time: ");
+        scanf("%d", &burst_time[i]);
+        processes[i] = i + 1;
     }
 
- }
-}
-
-int main(){
-
-    int i, n;
- printf("\nEnter the number of Processes: ");
- scanf("%d", &n);
- struct process p[n];
- for (i = 0; i < n; i++)
- {
- printf("\nEnter the arrival time for process %d:", i + 1);
- scanf("%d", &p[i].at);
- printf("\nEnter the burst time for process %d:", i + 1);
- scanf("%d", &p[i].bt);
- }
-
  
 
-
+   for(int i=1;i<n;i++){
+      for(int j = i+1;i<n;i++){
+         if(at[i]>))
+      }
+   }
+    return 0;
 }
