599e1682c5ec38ecd747c1fa2a6ddf5eb3721918
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-09-27 07:05:16 +0530

    .

--- Advance Operating system/lab/sjf.c
+++ Advance Operating system/lab/sjf.c
@@ -1,32 +1,94 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main() {
-    int n;
+#include <stdio.h>
+void calcAvgTime(int processes[], int n, int bt[], int at[]) {
+    int wt[n], tat[n],ct[n];
 
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
+ ct[0] = bt[0]; 
+ tat[0] = ct[0] - at[0];
+ wt[0]=tat[0]-bt[0];
+ for (int i = 1; i < n; i++)
+ {
+ ct[i] = ct[i - 1] + bt[i];
+ tat[i] = ct[i] - at[i];
+ wt[i]= tat[i] - bt[i];
+ }
 
-    int processes[n];
-    int arrival_time[n];
-    int burst_time[n];
-    int ct[n];
-
-    printf("Enter the arrival time and burst time for each process:\n");
+    float avg_wt = 0, avg_tat = 0;
     for (int i = 0; i < n; i++) {
-        printf("Process %d:\n", i + 1);
-        printf("Arrival Time: ");
-        scanf("%d", &arrival_time[i]);
-        printf("Burst Time: ");
-        scanf("%d", &burst_time[i]);
-        processes[i] = i + 1;
+        avg_wt += wt[i];
+        avg_tat += tat[i];
     }
+    avg_wt /= n;
+    avg_tat /= n;
 
- 
+    printf("Process\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], at[i], bt[i], wt[i], tat[i]);
+    }
+    printf("Average Waiting Time: %.2f\n", avg_wt);
+    printf("Average Turnaround Time: %.2f\n", avg_tat);
+}
 
-   for(int i=1;i<n;i++){
-      for(int j = i+1;i<n;i++){
-         if(at[i]>))
+
+
+int main() {
+  int i, j, n, temp;
+  int arrival_time[100];
+  int burst_time[100];
+  int processes[100];
+
+  printf("Enter the number of processes: ");
+  scanf("%d", &n);
+
+  for (i = 0; i < n; i++) {
+    printf("For Process %d\n", i + 1);
+    printf("Enter the arrival time: ");
+    scanf("%d", &arrival_time[i]);
+    printf("Enter the burst time: ");
+    scanf("%d", &burst_time[i]);
+    processes[i] = i + 1;
+  }
+
+
+
+  for (i = 0; i < n - 1; i++) {
+    for (j = i + 1; j < n; j++) {
+      if (arrival_time[i] > arrival_time[j]) {
+        temp = arrival_time[i];
+        arrival_time[i] = arrival_time[j];
+        arrival_time[j] = temp;
+
+        temp = burst_time[i];
+        burst_time[i] = burst_time[j];
+        burst_time[j] = temp;
+
+        temp = processes[i];
+        processes[i] = processes[j];
+        processes[j] = temp;
       }
-   }
-    return 0;
+    }
+  }
+  
+
+  for (i = 1; i < n - 1; i++) {
+    for (j = i + 1; j < n; j++) {
+      if (burst_time[i] > burst_time[j]) {
+        temp = arrival_time[i];
+        arrival_time[i] = arrival_time[j];
+        arrival_time[j] = temp;
+
+        temp = burst_time[i];
+        burst_time[i] = burst_time[j];
+        burst_time[j] = temp;
+
+        temp = processes[i];
+        processes[i] = processes[j];
+        processes[j] = temp;
+      }
+    }
+  }
+
+  calcAvgTime(processes, n, burst_time, arrival_time);
+
+
+  return 0;
 }
