a0650c7d7c5d6f6f4a708db04fd36ea28c43a816
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-11-01 08:02:50 +0530

    .

---
+++ Advance Operating system/lab/sstf.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+ int r,i,temp,j, h, seektime=0,cal,qu[r],mod[r];
+ printf("\nEnter the Requests:");
+ scanf("%d", &r);
+ for (i = 0; i < r; i++)
+ {
+ printf("\n Enter Cylinder %d value:", i);
+ scanf("%d", &qu[i]);
+ }
+ printf("\nEnter Starting Cylinder of Disk:");
+ scanf("%d", &h);
+ int n = r;
+
+ for (i=0;i<r;i++){
+   int mod[x];
+   for(j=0;j<n;j++){
+      mod[j]=fabs(h)-qu[j];
+
+       for (i = 0; i < r - 1; i++) 
+       {
+         for (j = i + 1; j < r; j++) 
+         {
+         if(mod[i]>mod[j])
+            {
+               temp = mod[i];
+               mod[i] = mod[j];
+               mod[j] = temp;
+
+               temp = qu[i];
+               qu[i] = qu[j];
+               qu[j] = temp;
+
+            }
+         }
+       }
+       h = qu[0];
+       x-=1;
+
+   }
+ }
+
+
+  
+ return 0;
+}
--- Advance Operating system/lab/sstfdisk.c
+++ Advance Operating system/lab/sstfdisk.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
 #include <math.h>
-int main()
-{
- int r,i,temp,j, s,ch, seektime=0,cal,qu[r],mod[r];
- printf("\nEnter the Requests:");
- scanf("%d", &r);
- for (int i = 0; i < r; i++)
- {
- printf("\n Enter Cylinder %d value:", i);
- scanf("%d", &qu[i]);
- }
- printf("\nEnter Starting Cylinder of Disk:");
- scanf("%d", &s);
- ch = s;
- for( i = 0; i<r;i++){
-    mod[i]= fabs(qu[i]-ch);
- }
- for( i = 0; i<r;i++){
-    printf("%d\n",mod[i]);
- }
-printf("--------------");
-   for (i = 0; i < r - 1; i++) {
-    for (j = i + 1; j < r; j++) {
-     if(mod[i]>mod[j])
-     {
-        temp = mod[i];
-        mod[i] = mod[j];
-        mod[j] = temp;
 
-        temp = qu[i];
-    qu[i] = qu[j];
-        qu[j] = temp;
+int main() {
+  int r,x ;
 
-     }
-      }
-    }
-   for( i = 0; i<r;i++){
-    printf("%d\n",mod[i]);
- }
-  
- return 0;
+  printf("\nEnter the Requests:");
+  scanf("%d", &r);
+  x = r+1;
+   int qu[x];
+   printf("head");
+   scanf("%d",&qu[0]);
+  for (int i = 1; i < x; i++) {
+    printf("enter the value %d :", i);
+    scanf("%d", &qu[i]);
+  }
+  for (int i = 0; i < x; i++) {
+    printf(" %d \n", qu);
+   
+  }
+
+
+
+
+  return 0;
 }
