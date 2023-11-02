09ac21c9e9b752a45811449c991ad734033ca4b7
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-11-02 07:03:20 +0530

    .

--- Advance Operating system/lab/sstfdisk.c
+++ Advance Operating system/lab/sstfdisk.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-  int r,x ;
+  int r,x ,i,j,k,temp;
 
   printf("\nEnter the Requests:");
   scanf("%d", &r);
@@ -14,13 +14,27 @@ int main() {
     printf("enter the value %d :", i);
     scanf("%d", &qu[i]);
   }
-  for (int i = 0; i < x; i++) {
-    printf(" %d \n", qu);
+ 
+ for (i = 0; i < x - 1; i++) 
+       {
+         for ( j = i + 1; j < x; j++) 
+         {
+         if(qu[i]>qu[j])
+            {
+               
+
+               temp = qu[i];
+               qu[i] = qu[j];
+               qu[j] = temp;
+
+            }
+         }
+       }
+
+ for (int i = 0; i < x; i++) {
+    printf(" %d \n", qu[i]);
    
   }
-
-
-
 
   return 0;
 }
