a5cbf51405cbbf4a732cc92499a3783f8aa1ec7b
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-11-22 15:19:39 +0530

    .

--- Advance Operating system/lab/sstfdisk.c
+++ Advance Operating system/lab/sstfdisk.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
-#include <math.h>
-
+#include <stdlib.h>
+#define max 1000
+// finding minimum distance
+int find_min_dist(int r[], int n, int head) {
+ int min_dist = max;
+ int index = -1;
+ for (int i = 0; i < n; i++) {
+ if (abs(r[i] - head) < min_dist) {
+ min_dist = abs(r[i] - head);
+ index = i;
+ }
+ }
+ return index;
+}
 int main() {
-  int r,x ,i,j,k,temp;
-
-  printf("\nEnter the Requests:");
-  scanf("%d", &r);
-  x = r+1;
-   int qu[x];
-   printf("head");
-   scanf("%d",&qu[0]);
-  for (int i = 1; i < x; i++) {
-    printf("enter the value %d :", i);
-    scanf("%d", &qu[i]);
-  }
- 
- for (i = 0; i < x - 1; i++) 
-       {
-         for ( j = i + 1; j < x; j++) 
-         {
-         if(qu[i]>qu[j])
-            {
-               
-
-               temp = qu[i];
-               qu[i] = qu[j];
-               qu[j] = temp;
-
-            }
-         }
-       }
-
- for (int i = 0; i < x; i++) {
-    printf(" %d \n", qu[i]);
-   
-  }
-
-  return 0;
+ int h, n, i, temp, r[15];
+ printf("\nEnter the number of requests: ");
+ scanf("%d", &n);
+ printf("\nEnter request values: ");
+ for (i = 0; i < n; i++) {
+ scanf("%d", &r[i]);
+ }
+printf("\nEnter the head position: ");
+ scanf("%d", &h);
+ int totalmovement= 0;
+ printf(" Visited Cylinders: ");
+ for (i = 0; i < n; i++) {
+ int index = find_min_dist(r, n, h);
+ if (index == -1)
+ break;
+ printf("%d ", r[index]);
+ totalmovement = totalmovement + abs(r[index] - h);
+ h = r[index];
+ r[index] = max; // visited
+ }
+ printf("\nTotal movement: %d", totalmovement);
+ return 0;
 }
