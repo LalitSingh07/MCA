6d7748603b706adf44f3f6370cd66183d4e80799
LalitSingh07 <lalitsingh99275910@gmail.com>
2023-12-17 11:12:44 +0530

    .

--- Advance Operating system/lab/test.py
+++ Advance Operating system/lab/test.py
@@ -1,11 +1,7 @@
-ls1 = [14]
-ls2 = [14,2,4,6,8]
-ls3=[]
-
-for x in ls1:
-    if x not in ls2:
-        ls3.append(x)
-for y in ls2:
-    if y not in ls1:
-        ls3.append(y)
-print(ls3)
+for i in range(6):
+    if i == 3:
+        continue
+    print(i)
+else:
+    print("finally executed")
+   
